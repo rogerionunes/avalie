919a2bb0ddeb8a9bbd25e235bfeffed5e69059d3
Rogério Nunes <rogerionunes90@gmail.com>
2022-07-18 22:59:14 -0300

    corrigido msg de sucesso apos salvar avaliacao

--- app/Http/Controllers/AvaliacaoController.php
+++ app/Http/Controllers/AvaliacaoController.php
@@ -136,7 +136,7 @@ class AvaliacaoController extends Controller
         
         DB::commit();
         
-        return redirect()->route('/')->withSuccess(['Avaliação feita com sucesso! :)']);
+        return redirect()->route('/')->with('success', 'Avaliação feita com sucesso!');
     }
     
     public function sessao(Request $request)
